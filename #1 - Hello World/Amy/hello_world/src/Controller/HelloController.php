6cf226c5a5b9d40701d929c8f002841f98088cc4
amyvs <amy@thinkshout.com>
2015-08-17 21:51:28 -0700

    Add my hello_world module (Amy).

---
+++ #1 - Hello World/Amy/hello_world/src/Controller/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\hello_world\Controller\HelloController.
+ */
+
+namespace Drupal\hello_world\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class HelloController extends ControllerBase {
+  public function content() {
+    return array(
+      '#type' => 'markup',
+      '#markup' => t('Hello, World!'),
+    );
+  }
+}
